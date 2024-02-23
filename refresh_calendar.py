ba8e18be157b28b7c2fffc06ae57ea0ed33ddb6a
Kyle Oman <kyle.a.oman@durham.ac.uk>
2024-02-23 13:41:25 +0000

    Initial attempt to use github token for calendar API.

--- refresh_calendar.py
+++ refresh_calendar.py
@@ -2,6 +2,7 @@
 
 import datetime
 import os.path
+import sys
 
 from google.auth.transport.requests import Request
 from google.oauth2.credentials import Credentials
@@ -11,6 +12,7 @@ from googleapiclient.errors import HttpError
 
 # If modifying these scopes, delete the file token.json.
 SCOPES = ["https://www.googleapis.com/auth/calendar.readonly"]
+token_file = sys.argv[1]
 
 
 def main():
@@ -21,18 +23,20 @@ def main():
     # The file token.json stores the user's access and refresh tokens, and is
     # created automatically when the authorization flow completes for the first
     # time.
-    if os.path.exists("token.json"):
-        creds = Credentials.from_authorized_user_file("token.json", SCOPES)
+    if os.path.exists(token_file):
+        creds = Credentials.from_authorized_user_file(token_file, SCOPES)
+    else:
+        raise FileNotFoundError("Missing token file.")
     # If there are no (valid) credentials available, let the user log in.
-    if not creds or not creds.valid:
-        if creds and creds.expired and creds.refresh_token:
-            creds.refresh(Request())
-        else:
-            flow = InstalledAppFlow.from_client_secrets_file("credentials.json", SCOPES)
-            creds = flow.run_local_server(port=0)
-        # Save the credentials for the next run
-        with open("token.json", "w") as token:
-            token.write(creds.to_json())
+    # if not creds or not creds.valid:
+    #     if creds and creds.expired and creds.refresh_token:
+    #         creds.refresh(Request())
+    #     else:
+    #         flow = InstalledAppFlow.from_client_secrets_file("credentials.json", SCOPES)
+    #         creds = flow.run_local_server(port=0)
+    # Save the credentials for the next run
+    # with open("token.json", "w") as token:
+    #     token.write(creds.to_json())
 
     try:
         service = build("calendar", "v3", credentials=creds)
